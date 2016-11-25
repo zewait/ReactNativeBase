14da06519a0a995b759c4e8ad625fe93db505c9d
世凡 黄 <773108248@qq.com>
2016-11-25 12:00:57 +0800

    feact: 评论组件 根据https://yanshuo.io/assets/player/?deck=5836a844c59e0d005778252f#/4 的数据和视图组件创建一个评论

--- index.android.js
+++ index.android.js
@@ -10,28 +10,51 @@ const Component = React.Component;
 import {
   AppRegistry,
   Text,
+  View,
+  Image,
 } from 'react-native';
 
 // 导入自己创建的模块要与node_modules分隔一行
 import formatDate from './src/formatDate';
 
-/*
-function Welcome(props) {
-  return <Text>Hello, {props.name}!</Text>;
-}
-*/
-
-class Welcome extends Component {
-  render() {
-    return <Text>Hello, {this.props.name}!</Text>;
-  }
+// 组件名必须大写
+function Comment(props) {
+  return (
+    <View>
+      <Image  
+        source={{uri: props.author.avatarUrl}}
+        style={style.avatar}
+      />
+      <Text>{props.author.name}</Text>
+      <Text>{props.text}</Text>
+      <Text>{formatDate(props.date)}</Text>
+    </View>
+  );
 }
 
 export default class ReactNativeBase extends Component {
   render() {
     return (
-      <Welcome name="Class Component" />
+      <Comment {...COMMENT_DATA} />
     );
+  }
+}
+
+
+// 定义常量都是大写跟下划线
+const COMMENT_DATA = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+} 
+
+let style = {
+  avatar: {
+    height: 64,
+    width: 64,
   }
 }
 
