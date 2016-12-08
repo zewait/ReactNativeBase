b235c94fb0e859cd79eeae54d4b35d0d6063b4ff
世凡 黄 <773108248@qq.com>
2016-12-08 15:29:14 +0800

    feat: setState第二种传值方式

--- src/component/Clock.js
+++ src/component/Clock.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import {
-  Text
+  Text,
+  View,
 } from 'react-native';
 
 export default class Clock extends Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date() };
+    this.state = { date: new Date() , times: 0};
   }
 
   componentDidMount() {
@@ -22,9 +23,18 @@ export default class Clock extends Component {
 
   tick() {
     this.setState({ date: new Date });
+    // 另一种setState使用
+    // 应用场景,需要用到上一次状态
+    // 因为异步环境,直接引用this.state.times保证不了准确性
+    this.setState((prevState, props) => ({
+      times: prevState.times + 1
+    }));
   }
 
   render() {
-    return <Text>{this.state.date.toLocaleTimeString()}</Text>;
+    return (<View>
+      <Text>{this.state.date.toLocaleTimeString()}</Text>
+      <Text>{this.state.times}</Text>
+    </View>);
   }
 }
